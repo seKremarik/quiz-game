0cc0a5313ce0225bf844cf2e9f52cd56c177b30c
Sebastian Kremarik <emilio.kremarik@gmail.com>
2024-01-19 20:25:03 -0300

    Finished the basic structure of the program
    Added a counter for questions and correct answers.
    Now can handle flags like -h and -csv.

--- src/files/questions.go
+++ src/files/questions.go
@@ -1,21 +1,58 @@
 package files
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-func PrintQuestion(questions [][]string) {
-	fmt.Println("Write /exit to terminate process")
-	for _, line := range questions {
-		var input string
+func ManageArgs() string {
+	csvFilePath := flag.String("csv", "src/data/problems.csv", "CSV file in the format 'question,answer'")
+	help := flag.Bool("h", false, "Show help message")
 
-		for input != line[1] {
-			fmt.Printf("%v   -> ", line[0])
-			fmt.Scan(&input)
+	flag.Parse()
 
-			if input == "/exit" {
-				return
-			}
-		}
+	if *help {
+		printHelp()
+		os.Exit(1)
 	}
+
+	absPath, err := filepath.Abs(*csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return absPath
+}
+
+func PrintQuestions(questions [][]string) {
+	fmt.Println("Write /exit to terminate process")
+	var input string
+	var totalQuestions, totalCorrect int16
+	for _, line := range questions {
+
+		// Print the question
+		fmt.Print(line[0], " > ")
+
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Check answer
+		if input == line[1] {
+			totalCorrect++
+		}
+
+		totalQuestions++
+	}
+
+	fmt.Printf("%d questions were asked\n", totalQuestions)
+	fmt.Printf("%d were correct answers", totalCorrect)
+}
+
+func printHelp() {
+	fmt.Printf("Usage of ./quiz:\n-csv string\n\tA CSV file in the format of 'question,answer' (default: 'problems.csv')\n-limit int\n\tThe time limit for the quiz is second (default 10)\n")
 }
--- src/main/main.go
+++ src/main/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"log"
-	"path/filepath"
 	quiz "quiz-game/src/files"
 )
 
 func main() {
-	file, err := filepath.Abs("src/data/problems.csv")
-	if err != nil {
-		log.Panic(err)
-	}
-
+	file := quiz.ManageArgs()
 	lines := quiz.OpenCSV(file)
 
-	quiz.PrintQuestion(lines)
+	quiz.PrintQuestions(lines)
 }
